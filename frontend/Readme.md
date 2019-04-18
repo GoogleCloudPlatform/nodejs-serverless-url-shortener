61f8acb1efff016d25ddeffb2cf74159d8e4306e
Myles Borins <mylesborins@google.com>
2019-04-18 14:28:03 -0400

    scaffold of frontend

---
+++ frontend/Readme.md
@@ -0,0 +1,28 @@
+# URL Shortener web frontend
+
+Express.js web frontend to create shortlinks and forward requests
+
+## Dependencies
+
+- Google App Engine
+- Cloud SQL
+
+## Development
+
+Run locally with pretty printed bunyan logs with `npm run dev`.
+
+## Deploy
+
+`npm run deploy`
+
+## Notes
+
+To run this locally you will need to set the following ENVARS
+
+* GCLOUD\_PROJECT
+  - Name of your project
+* GOOGLE\_APPLICATION\_CREDENTIALS
+  - Path to local service account credentials
+  - more details at https://cloud.google.com/docs/authentication/getting-started
+
+## TODO
---
+++ frontend/app.js
@@ -0,0 +1,64 @@
+/*
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+'use strict';
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+// const favicon = require('serve-favicon');
+const path = require('path');
+
+const index = require('./routes/index');
+const logger = require('./logger');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error(`${req.originalUrl} Not Found`);
+  err.status = 404;
+  logger.warn(err);
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // log the error
+  if (err.status && err.status !== 404) logger.error(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
