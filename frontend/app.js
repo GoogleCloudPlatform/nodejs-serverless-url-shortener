5e58888f919fb07bd121744cc1939b5a43430429
Myles Borins <mylesborins@google.com>
2019-05-03 04:30:29 -0700

    frontend: add favicon

--- frontend/app.js
+++ frontend/app.js
@@ -19,7 +19,7 @@ limitations under the License.
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const path = require('path');
 
 const fetch = require('node-fetch');
@@ -39,7 +39,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
