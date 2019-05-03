ab56bc04d80ecb3662e422a68948890ce4962f03
Myles Borins <mylesborins@google.com>
2019-05-02 17:48:31 -0700

    four-oh-four: add license header

--- four-oh-four/deploy.sh
+++ four-oh-four/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Copyright 2019 Google LLC
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
 
 PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
 
